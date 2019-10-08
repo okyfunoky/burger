ebdd18d7f9df160701435d4863e02338578709b5
Okyfunoky <okyfunoky@hotmail.com>
2019-10-08 12:49:21 -0700

    DB seeds

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (name) VALUES ('Royal With Cheese');
+INSERT INTO burgers (name) VALUES ('Quarter Pounder with Cheese');
+INSERT INTO burgers (name) VALUES ('Bacon Burger');
+INSERT INTO burgers (name) VALUES ('BBQ Jalapeno Burger');
